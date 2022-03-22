e78de91c2e824c6749d78f365902d44dca2a64f8
emselis <ezequiel.selis@gmail.com>
2022-03-22 13:29:53 -0300

    Conexion a SQL Server usando dependencia jdbc con Maven

---
+++ abm_maven/ABM-eclipse-maven/src/main/java/net/codejava/sql/JavaConnect2SQL.java
@@ -0,0 +1,26 @@
+package net.codejava.sql;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class JavaConnect2SQL {
+
+	public static void main(String[] args) {
+		String url = "jdbc:sqlserver://ems-nqn\\sqlexpress;databaseName=java_practica;trustServerCertificate=true"; // integratedSecurity=true;hostNameInCertificate=null";
+		String user = "valen";
+		String password = "tino17";
+		
+		try {
+			
+			Connection cn = DriverManager.getConnection(url, user, password);
+			// Otra: DriverManager.getConnection(url, user, password)
+
+			System.out.println("Correcta conexion con el Servidor SQL.");
+			
+		} catch (SQLException e) {
+			System.out.println("No se pudo conectar al Servidor SQL.");
+			e.printStackTrace();
+		}
+	}		
+}
