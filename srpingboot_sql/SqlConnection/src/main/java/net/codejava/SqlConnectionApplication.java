9a1e6cf4dfdcf2fe71e6b000a61714ed70d8a64e
emselis <ezequiel.selis@gmail.com>
2022-03-25 01:02:31 -0300

    Proyecto creado con SpringBoot, probando conexion a mi base SQL, insertando una persona en una tabla

---
+++ srpingboot_sql/SqlConnection/src/main/java/net/codejava/SqlConnectionApplication.java
@@ -0,0 +1,30 @@
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class SqlConnectionApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlConnectionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO Personas (id, nombre, apellido, edad, direccion) VALUES (?, ?, ?, ?, ?)";
+		int result = jdbcTemplate.update(sql, 3, "Eze", "Martin", 40, "Neuquen 123");
+		
+		if (result > 0) {
+			System.out.println("Una nueva linea se agrego correctamente.");
+		}
+	}
+	
+
+}
